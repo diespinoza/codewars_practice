b486dfe2f3b2b23acf34ad617d6ef403cc71f3f0
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-07-25 14:07:04 -0400

    Finished my first kata! "What's the real floor"

--- 8kyu/getRealFloor.js
+++ 8kyu/getRealFloor.js
@@ -1,3 +1,12 @@
-function getRealFloor(n){
-
+//Write a function that giver a floor in the american system returns the floor in the european system
+function getRealFloor(n) {
+  if (n>=13){
+    return n-2;
+  }
+  else if (n<=0){
+    return n;
+  }
+  else {
+    return n-1;
+  }
 }
