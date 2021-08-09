5d9b48ad690b42c73f8103eb07d5a7da4bc0b9e5
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-08-09 19:40:20 -0400

    8ky exercise Alan Partridge II

---
+++ 8kyu/appleTurnover.js
@@ -0,0 +1,8 @@
+//function that checks if x squared is greater than 100 and returns
+function apple(x){
+  if (x*x > 1000){
+    return "It's hotter than the sun!!"
+  } else {
+    return "Help yourself to a honeycomb Yorkie for the glovebox."
+  }
+}
