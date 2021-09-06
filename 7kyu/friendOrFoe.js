4919f12f59cef3d8ebe5009951a44636b73334b0
Diego <diego.espinoza_rodriguez@protonmail.com>
2021-09-06 14:09:10 -0400

    7lyu kata

---
+++ 7kyu/friendOrFoe.js
@@ -0,0 +1,5 @@
+//Filter an array of strings to only return the strings with 4 characters
+function friend(friends){
+  //Find the length of each string in the array
+ return friends.filter(e => e.length == 4);
+}
